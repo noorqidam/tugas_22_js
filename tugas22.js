46d8f987120d1c24ac04209ba969a00f5a5831f2
noorqidam <noorqidam@gmail.com>
2019-09-03 15:17:06 +0700

    Tugas 22 JS
    Tugas ini saya kumpulkan untuk Niomic

---
+++ tugas22.js
@@ -0,0 +1,7 @@
+function tugas22() {
+  var data = ["Saya","ingin","belajar","bersama"]
+  data.forEach(function (item, index){
+    console.log("item : ",item,"index ke ",index);
+  })
+}
+tugas22();
